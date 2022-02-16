4ebf215fe5c1cd32721df8c7b095cc4656ccaf2b
Lanie Williams <lanie_williams@baylor.edu>
2022-02-16 14:58:34 -0600

    Quiz

--- weaponProgram.py
+++ weaponProgram.py
@@ -38,35 +38,39 @@ for record in weapons_file:
     name = record[0]
     speed = record[1]
     range_w = record[2]
-
+    
     # create an instance of the weapon object using the 
     # specs from the csv file (name,speed and range) 
-    weapon = w.weapon(name,speed,range_w)
+    my_weapon = w.Weapon(name,speed,range_w,)
 
     # append the name and bullet count to 'weapons_dict'
     
-    weapons_dict[name] = weapon.get_bullets()
-
+    weapons_dict[name] = my_weapon.get_bullets()
+    print('----------------')
     # print out the name of the weapon using the appropriate method of the object 
-    print('Name: ',weapon.get_name(),'\r')
+    print('Name: ',my_weapon.get_name(),'\r')
     # print out the speed of the weapon using the appropriate method of the object
-    print('Speed: ',weapon.get_speed(),'\r')
+    print('Speed: ',my_weapon.get_speed(),'\r')
     # print out the range of the weapon using the appropriate method of the object
-    print('Range: ',weapon.get_range(),'\r')
+    print('Range: ',my_weapon.get_range(),'\r')
     # print out the number of bullets of the weapon using the appropriate method of the object
-    print('Bullets: ',weapon.get_bullets(),'\r')
+    print('Bullets: ',my_weapon.get_bullets(),'\r')
 
     #use an input statement to halt the program and wait for the user - 
     input("Press any key to fire the weapon")
     
+    print()
+
 
     # use an appropriate loop to keep firing the weapon until all bullets run out
-    for i in range(weapon.get_bullets):
+    for i in range(my_weapon.get_bullets()):
         # call the appropriate method to fire a bullet
-        if weapon.get_status() == 'Active':
-            weapon.fire_bullets()
+        if my_weapon.get_status() == 'Active':
+            my_weapon.fire_bullet()
         # print out the bullet count every time the weapon is fired
-            print('bullets remaining...',weapon.get_bullets())
+            print('bullets remaining...',my_weapon.get_bullets(),end=''+'\r')
+    print()
+   
 
     
 
